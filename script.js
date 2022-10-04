e0d00603bc89f86dcfc662a402e6d5b2b6587307
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-03 23:29:27 -0300

    change full grid color

--- script.js
+++ script.js
@@ -7,14 +7,107 @@ const CELL_SELECTOR = {
     LINE_5: ".line-5",
 }
 
-function changeColor(e) {
-    console.log("changeColor");
-    console.log(e.value);
-    const colorSelected = e.value;
+const COLORS = [
+    {
+        name: "White",
+        code: "#FFFFFF"
+    },
+    {
+        name: "Silver",
+        code: "#C0C0C0"
+    },
+    {
+        name: "Gray",
+        code: "#808080"
+    },
+    {
+        name: "Black",
+        code: "#000000"
+    },
+    {
+        name: "Red",
+        code: "#FF0000"
+    },
+    {
+        name: "Maroon",
+        code: "#800000"
+    },
+    {
+        name: "Yellow",
+        code: "#FFFF00"
+    },
+    {
+        name: "Olive",
+        code: "#808000"
+    },
+    {
+        name: "Lime",
+        code: "#00FF00"
+    },
+    {
+        name: "Green",
+        code: "#008000"
+    },
+    {
+        name: "Aqua",
+        code: "#00FFFF"
+    },
+    {
+        name: "Teal",
+        code: "#008080"
+    },
+    {
+        name: "Blue",
+        code: "#0000FF"
+    },
+    {
+        name: "Navy",
+        code: "#000080"
+    },
+    {
+        name: "Fuchsia",
+        code: "#FF00FF"
+    },
+    {
+        name: "Purple",
+        code: "#800080"
+    },
+]
 
-    let allgrids = document.querySelectorAll(CELL_SELECTOR.ALL);
+// Utils
+
+function changeBackgroundColor(selector, color) {
+    let allgrids = document.querySelectorAll(selector);
     allgrids.forEach((item) => {
-        item.style.backgroundColor = e.value;
+        item.style.backgroundColor = color;
     })
 }
 
+function changeBackgroundColorList(selectorList, color) {
+    selectorList.forEach((selector) => {
+        changeBackgroundColor(selector, color);
+    })
+}
+
+
+
+function fillSelectOption(selectComponentId, optionList) {
+    var select = document.getElementById(selectComponentId);
+    select.innerHTML = "";
+    optionList.forEach((color) => {
+        console.log(color);
+        var option = document.createElement("option");
+        option.value = color.code;
+        option.innerHTML = color.name;
+        select.appendChild(option);
+    });
+}
+
+
+const changeAllCellsColor = (e) => changeBackgroundColorList([CELL_SELECTOR.ALL, '.color-all-container'], e.value);
+
+function loadColorSelect() {
+    fillSelectOption("color-all", COLORS);
+}
+
+loadColorSelect();
