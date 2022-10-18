696830ac39a8441f40f837c0410055e30ddde564
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-18 20:34:59 -0300

    selector improvments

--- script.js
+++ script.js
@@ -7,6 +7,33 @@ const CELL_SELECTOR = {
     LINE_5: ".line-5",
 }
 
+const COLORPICKER_SELECTOR = {
+    ALL: "#colorpicker-all",
+    LINE_1: "#colorpicker-1",
+    LINE_2: "#colorpicker-2",
+    LINE_3: "#colorpicker-3",
+    LINE_4: "#colorpicker-4",
+    LINE_5: "#colorpicker-5",
+}
+
+const SIMPLE_COLORPICKER_SELECTOR = {
+    ALL: "#simple-colorpicker-all",
+    LINE_1: "#simple-colorpicker-1",
+    LINE_2: "#simple-colorpicker-2",
+    LINE_3: "#simple-colorpicker-3",
+    LINE_4: "#simple-colorpicker-4",
+    LINE_5: "#simple-colorpicker-5",
+}
+
+const COLORPICKER_CONTAINER = {
+    ALL: "#color-container-all",
+    LINE_1: "#color-container-1",
+    LINE_2: "#color-container-2",
+    LINE_3: "#color-container-3",
+    LINE_4: "#color-container-4",
+    LINE_5: "#color-container-5",
+}
+
 const COLORS = [
     {
         name: "White",
@@ -89,10 +116,16 @@ function changeBackgroundColorList(selectorList, color) {
     })
 }
 
+function getColorPickerColor(colorPickerId) {
+    const colorpicker = document.querySelector(colorPickerId);
+
+    return colorpicker.value;
+}
+
 
 
 function fillSelectOption(selectComponentId, optionList) {
-    var select = document.getElementById(selectComponentId);
+    var select = document.querySelector(selectComponentId);
     select.innerHTML = "";
     optionList.forEach((color) => {
         console.log(color);
@@ -104,10 +137,23 @@ function fillSelectOption(selectComponentId, optionList) {
 }
 
 
-const changeAllCellsColor = (e) => changeBackgroundColorList([CELL_SELECTOR.ALL, '.color-all-container'], e.value);
 
-function loadColorSelect() {
-    fillSelectOption("color-all", COLORS);
+const changeAllCellsColor1 = (e) => changeBackgroundColorList([CELL_SELECTOR.ALL, COLORPICKER_CONTAINER.ALL], e.value);
+
+function changeCellsColorAll () {
+    // get values from color picker
+    const color1 = getColorPickerColor(SIMPLE_COLORPICKER_SELECTOR.ALL);
+    const color2 = getColorPickerColor(COLORPICKER_SELECTOR.ALL);
+
+    changeBackgroundColorList([CELL_SELECTOR.ALL, COLORPICKER_CONTAINER.ALL], color1);
 }
 
-loadColorSelect();
+function loadColorSelect() {
+    fillSelectOption(SIMPLE_COLORPICKER_SELECTOR.ALL, COLORS);
+}
+
+function LoadAll() {
+    loadColorSelect();
+}
+
+LoadAll();
