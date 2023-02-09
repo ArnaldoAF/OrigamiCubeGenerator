7d7a8f7857e9cd2066fb12a985c2ef61648e9828
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-02-09 17:27:45 -0300

    toggle color picker modules

--- scripts/index.js
+++ scripts/index.js
@@ -29,24 +29,8 @@ import fillSelectOption from './fillSelectOption.js';
 
 import { setImagePreviewEvent, deletePreviewImage } from './imagePreview.js';
 
+import toggleColorPicker from './toggleColorPicker.js';
 
-// toggle ColorPicker
-
-/**
- * Alternates the colorPickers showing and hiding
- *
- * @param {array<string>} selectorColorPickerList List of the colorPicker's selectors that will be toggled
- */
-function toggleColorPicker(selectorColorPickerList) {
-    const [colorpicker1, colorpicker2] = getColorPickerList(selectorColorPickerList);
-
-    colorpicker1.classList.toggle("show");
-    colorpicker2.classList.toggle("show");
-
-    colorpicker1.classList.toggle("hide");
-    colorpicker2.classList.toggle("hide");
-
-}
 
 function toggleColorPickerAll() {
     toggleColorPicker([SIMPLE_COLORPICKER_SELECTOR.ALL, COLORPICKER_SELECTOR.ALL]);
