718ff676e797baddccdf93393f7794c3606caa55
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-02-14 22:14:06 -0300

    change the cell

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,10 +1,37 @@
 export const CELL_SELECTOR = {
     ALL: ".origami-cell",
+
     LINE_1: ".line-1",
     LINE_2: ".line-2",
     LINE_3: ".line-3",
     LINE_4: ".line-4",
     LINE_5: ".line-5",
+
+    CELL_1: "#cell-1",
+    CELL_2: "#cell-2",
+    CELL_3: "#cell-3",
+    CELL_4: "#cell-4",
+    CELL_5: "#cell-5",
+    CELL_6: "#cell-6",
+    CELL_7: "#cell-7",
+    CELL_8: "#cell-8",
+    CELL_9: "#cell-9",
+    CELL_10: "#cell-10",
+    CELL_11: "#cell-11",
+    CELL_12: "#cell-12",
+    CELL_13: "#cell-13",
+    CELL_14: "#cell-14",
+    CELL_15: "#cell-15",
+    CELL_16: "#cell-16",
+    CELL_17: "#cell-17",
+    CELL_18: "#cell-18",
+    CELL_19: "#cell-19",
+    CELL_20: "#cell-20",
+    CELL_21: "#cell-21",
+    CELL_22: "#cell-22",
+    CELL_23: "#cell-23",
+    CELL_24: "#cell-24",
+    CELL_25: "#cell-25"
 }
 
 export const COLORPICKER_SELECTOR = "#colorpicker-all";
@@ -16,11 +43,38 @@ export const COLORPICKER_CONTAINER = "#color-container-all";
 
 export const OPEN_MODAL_SELECTORS = {
     ALL: "#openModalAll",
+
     LINE_1: "#openModalLine1",
     LINE_2: "#openModalLine2",
     LINE_3: "#openModalLine3",
     LINE_4: "#openModalLine4",
     LINE_5: "#openModalLine5",
+
+    CELL_1: "#openModalCell1",
+    CELL_2: "#openModalCell2",
+    CELL_3: "#openModalCell3",
+    CELL_4: "#openModalCell4",
+    CELL_5: "#openModalCell5",
+    CELL_6: "#openModalCell6",
+    CELL_7: "#openModalCell7",
+    CELL_8: "#openModalCell8",
+    CELL_9: "#openModalCell9",
+    CELL_10: "#openModalCell10",
+    CELL_11: "#openModalCell11",
+    CELL_12: "#openModalCell12",
+    CELL_13: "#openModalCell13",
+    CELL_14: "#openModalCell14",
+    CELL_15: "#openModalCell15",
+    CELL_16: "#openModalCell16",
+    CELL_17: "#openModalCell17",
+    CELL_18: "#openModalCell18",
+    CELL_19: "#openModalCell19",
+    CELL_20: "#openModalCell20",
+    CELL_21: "#openModalCell21",
+    CELL_22: "#openModalCell22",
+    CELL_23: "#openModalCell23",
+    CELL_24: "#openModalCell24",
+    CELL_25: "#openModalCell25"
 }
 
 export const COLORS = [
