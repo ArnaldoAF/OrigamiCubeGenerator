7aad0f070647a3eec53654d94638a5459fc784f8
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-07-29 10:02:24 -0300

    Read me | font generator fix

--- scripts/presetsUtils/fontGenerator.js
+++ scripts/presetsUtils/fontGenerator.js
@@ -5,7 +5,7 @@ const alphabet = "0123456789abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
 const fs = require("fs");
 const path = require("path");
 
-const fontFolder = "super_mario64_font";
+const fontFolder = "super_mario64_font"; // <-- Modify the folder's name
 
 const root = process.cwd();
 const fontPath = path.join(
@@ -18,29 +18,64 @@ const fontPath = path.join(
 
 console.log(root);
 
+// MODIFY THE 3 VARIABLES BELLOW:
+
 const fontPrefix = "SM64_font"; // Object's Name
-const fontPrefixConst = "SM64_FONT"; // variable's name from the constants.js file
-const fontPrefixName = "SM64 Font"; // Name that will appear on the UI
+const fontPrefixConst = "SM64_FONT"; // variable's name from the constants.js file | its the same prefix as the images
+const fontPrefixName = "SM64 Font"; // Name that will appear on the UI(the select on the HTML) | Use a User friendy name
 
 // cont variable generator
 // This function will create variable declarations to put on the constants.js file
-let final_lines = alphabet
+const constantVariables = alphabet
   .map((letter) => {
-    const vName = `${fontPrefixConst}_${letter}: `;
-    const vValue =
-      "`${BLOCKS_PATH}/${SM64_FONT_BLOCKS_PATH}/SM64_font_" + letter + "`\n";
-
-    vName.concat("`${BLOCKS_PATH}/", "");
-
-    const a = `${fontPrefixConst}_${letter}: \`\${BLOCKS_PATH}/\${${fontPrefixConst}_BLOCKS_PATH}/${fontPrefix}_${letter}\``;
-    return a;
+    return `${fontPrefixConst}_${letter}: \`\${BLOCKS_PATH}/\${${fontPrefixConst}_BLOCKS_PATH}/${fontPrefix}_${letter}\``;
   })
-  .join();
+  .join(", \n");
 
+console.log("-------------------------------------------------");
 console.log(
   "Constants: (put this variables on the constant.js file, on the BLOCKS variable)"
 );
-console.log(final_lines);
+console.log("");
+console.log(constantVariables);
+
+// import generator
+// This function will create variable import to put on the presets.js file
+const importToPresetsLine = alphabet
+  .map((letter) => {
+    return `import ${fontPrefix}_${letter} from "./presets/${fontFolder}/${fontPrefix}_${letter}.js"`;
+  })
+  .join("; \n");
+
+console.log("-------------------------------------------------");
+console.log(
+  "Imports: (put this lines on the presets.js file, with the other imports)"
+);
+console.log("");
+console.log(importToPresetsLine);
+console.log("-------------------------------------------------");
+
+// variable presets generator
+// This function will create variable list to put on the presets.js file
+const presetList = alphabet
+  .map((letter) => {
+    return `${fontPrefix}_${letter}`;
+  })
+  .join(", \n");
+
+const presetListDeclaration = `
+const ${fontPrefix}_Blocks = [
+  ${presetList}
+]
+`;
+
+console.log("-------------------------------------------------");
+console.log(
+  "Preset List: (put this lines on the presets.js file, wiht the other array declation. Dont forget to include it on the presetList variable)"
+);
+console.log("");
+console.log(presetListDeclaration);
+console.log("-------------------------------------------------");
 
 // Create all preSet files for fonts
 alphabet.forEach((letter) => {
