a4e402ceb2c2e56b6e61f003ab38f8fca967f9d1
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-09-05 09:33:00 -0300

    fix: pre sets minecraft

--- scripts/presetsUtils/presets.js
+++ scripts/presetsUtils/presets.js
@@ -87,6 +87,10 @@ import MINE_pumpkin from "./presets/minecraft/MINE_pumpkin.js";
 import MINE_lapis_block from "./presets/minecraft/MINE_lapis_block.js";
 import MINE_oak_planks from "./presets/minecraft/MINE_oak_planks.js";
 import MINE_acacia_planks from "./presets/minecraft/MINE_acacia_planks.js";
+import MINE_bamboo_planks from "./presets/minecraft/MINE_bamboo_planks.js";
+import MINE_birch_planks from "./presets/minecraft/MINE_birch_planks.js";
+import MINE_dark_oak_planks from "./presets/minecraft/MINE_dark_oak_planks.js";
+import MINE_jungle_planks from "./presets/minecraft/MINE_jungle_planks.js";
 
 import SM64_font_0 from "./presets/super_mario64_font/SM64_font_0.js";
 import SM64_font_1 from "./presets/super_mario64_font/SM64_font_1.js";
@@ -124,11 +128,6 @@ import SM64_font_W from "./presets/super_mario64_font/SM64_font_W.js";
 import SM64_font_X from "./presets/super_mario64_font/SM64_font_X.js";
 import SM64_font_Y from "./presets/super_mario64_font/SM64_font_Y.js";
 import SM64_font_Z from "./presets/super_mario64_font/SM64_font_Z.js";
-
-import MINE_bamboo_planks from "./presets/minecraft/MINE_bamboo_planks.js";
-import MINE_birch_planks from "./presets/minecraft/MINE_birch_planks.js";
-import MINE_dark_oak_planks from "./presets/minecraft/MINE_dark_oak_planks.js";
-import MINE_jungle_planks from "./presets/minecraft/MINE_jungle_planks.js";
 
 const Minecraft_Blocks = [
   MINE_bamboo,
